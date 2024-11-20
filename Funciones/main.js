a0f9732cd9268cabf76bc9401b9eaffbc3fa6a38
Tavoluis2552 <siancasgustavo25@gmail.com>
2024-11-19 23:49:05 -0500

    corrección de audio

--- Funciones/main.js
+++ Funciones/main.js
@@ -1,9 +1,10 @@
-window.addEventListener('load', () => {
+document.getElementById('comenzarBtn').addEventListener('click', () => {
     const audio = document.getElementById('audioBienvenida');
-    
+
+    // Reproduce el audio al hacer clic
     audio.play().then(() => {
-        console.log('El audio se está reproduciendo automáticamente.');
+        console.log('Audio reproduciéndose tras el clic.');
     }).catch((error) => {
-        console.error('No se pudo reproducir automáticamente:', error);
+        console.error('Error al reproducir el audio:', error);
     });
 });
